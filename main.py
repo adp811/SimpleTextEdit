8114ab03c309364b83222d8671a5ea179c105993
Aryan Patel <adp2582@gmail.com>
2022-08-20 23:01:18 -0400

    added functionality to change font style and added save warning for program exit

--- main.py
+++ main.py
@@ -17,9 +17,17 @@ class SimpleTextEditGUI(QMainWindow):
         self.action18pt.triggered.connect(lambda: self.change_font_size(18))
         self.action24pt.triggered.connect(lambda: self.change_font_size(24))
 
+        # handle font style actions
+        self.actionArial.triggered.connect(lambda: self.change_font_style("Arial"))
+        self.actionTimes.triggered.connect(lambda: self.change_font_style("Times"))
+        self.actionHelvetica.triggered.connect(lambda: self.change_font_style("Helvetica"))
+
         # handle file actions
         self.actionOpen.triggered.connect(self.open_file_local)
         self.actionSave.triggered.connect(self.save_file_local)
+
+        # handle program exit/close
+        self.actionClose.triggered.connect(exit)
 
     def open_file_local(self):
         options = QFileDialog.Options()
@@ -38,8 +46,33 @@ class SimpleTextEditGUI(QMainWindow):
             with open(filename, "w") as f:
                 f.write(self.mainTextEdit.toPlainText())
 
+    def closeEvent(self, QCloseEvent):
+        dialogue = QMessageBox()
+        dialogue.setText("Do you want to save this file before closing?")
+        dialogue.addButton("Yes", QMessageBox.YesRole)
+        dialogue.addButton("No", QMessageBox.NoRole)
+        dialogue.addButton("Cancel", QMessageBox.RejectRole)
+
+        response = dialogue.exec()
+
+        # continue exit if response is == 1 (NoRole)
+
+        if response == 0:
+            self.save_file_local()
+            QCloseEvent.accept()
+
+        elif response == 2:
+            QCloseEvent.ignore()
+
     def change_font_size(self, font_size):
-        self.mainTextEdit.setFont(QFont("Arial", font_size))
+        font = self.mainTextEdit.font()
+        font_style = font.family()
+        self.mainTextEdit.setFont(QFont(font_style, font_size))
+
+    def change_font_style(self, font_style):
+        font = self.mainTextEdit.font()
+        font_size = font.pointSize()
+        self.mainTextEdit.setFont(QFont(font_style, font_size))
 
     def set_window_title(self, window_title):
         self.setWindowTitle(window_title)
